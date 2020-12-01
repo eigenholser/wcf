9abac197bcdc473f43c288a57d0518200320b4de
Scott Overholser <eigenholser@users.noreply.github.com>
2020-11-30 18:16:38 -0700

    Remove unused code

--- src/main/java/com/zyx2/practice/wcf/repository/PhoneRepository.java
+++ src/main/java/com/zyx2/practice/wcf/repository/PhoneRepository.java
@@ -6,16 +6,12 @@ import java.util.List;
 import javax.enterprise.context.ApplicationScoped;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Join;
-import javax.persistence.criteria.JoinType;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
 import com.zyx2.practice.wcf.model.Phone;
-import com.zyx2.practice.wcf.model.Phone_;
 import com.zyx2.practice.wcf.model.Usage;
 import com.zyx2.practice.wcf.model.Usage_;
 
@@ -33,11 +29,6 @@ public class PhoneRepository {
 		em.persist(phone);
 		return phone;
 	}
-	
-//	public List<Phone> findAll() {
-//		TypedQuery<Phone> query = em.createQuery("SELECT p FROM Phone p ORDER BY p.employeeName DESC", Phone.class);
-//		return query.getResultList();
-//	}
 	
 	public List<Phone> findAllPhones() {
 		CriteriaBuilder cb = em.getCriteriaBuilder();
